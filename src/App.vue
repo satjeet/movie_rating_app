ae93d90a12979bf92cc805365380e2e959aff358
Arturo <arturo.alfaro87@gmail.com>
2018-07-12 10:15:05 -0400

    Agregada dumm info y es esqueleto del catalogo de peliculas

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer fixed v-model="drawer" app >
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+    >
       <v-list dense>
         <router-link v-bind:to="{ name: 'Home' }" class="side_bar_link">
           <v-list-tile>
@@ -23,6 +27,10 @@
     <v-toolbar color="indigo" dark fixed app>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
       <v-toolbar-title>Home</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat v-bind:to="{ name: 'AddMovie' }">Add Movie</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
     <v-content>
       <v-container fluid>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-import BootstrapVue from 'bootstrap-vue';
 
+import BootstrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import App from './App';
@@ -21,5 +20,4 @@ new Vue({
   router,
   components: { App },
   template: '<App/>',
-  //
 });
