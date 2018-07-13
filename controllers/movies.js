9eead87e4b2a7c972ab0675c9dca0b8c1cb9bbe3
Arturo <arturo.alfaro87@gmail.com>
2018-07-13 02:48:12 -0400

    comienzo de la autentificacion

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,5 +1,5 @@
 const MovieSchema = require('../models/Movie.js');
-
+const Rating = require("../models/Rating");
 module.exports.controller = (app) => {
   // fetch all movies
   app.get('/movies', (req, res) => {
@@ -19,6 +19,23 @@ module.exports.controller = (app) => {
      });
    });
 
+   // rate a movie
+     app.post('/movies/rate/:id', (req, res) => {
+       const rating = new Rating({
+         movie_id: req.params.id,
+         user_id: req.body.user_id,
+         rate: req.body.rate,
+       })
+
+       rating.save(function (error, rating) {
+         if (error) { console.log(error); }
+         res.send({
+           movie_id: rating.movie_id,
+           user_id: rating.user_id,
+           rate: rating.rate
+         })
+       })
+     })
   // add a new movie
   app.post('/movies', (req, res) => {
     const newMovie = new MovieSchema({
--- src/components/Movie.vue
+++ src/components/Movie.vue
@@ -8,7 +8,7 @@
             <span class="grey--text">{{ movie.release_year }} ‧ {{ movie.genre }}</span>
           </div>
         </v-card-title>
-        <h6 class="card-title">Rate this movie</h6>
+        <h6 class="card-title" @click="rate">Rate this movie</h6>
         <v-card-text>
           {{ movie.description }}
         </v-card-text>
@@ -16,8 +16,34 @@
     </v-flex>
   </v-layout>
 </template>
+
 <script>
 import axios from 'axios';
+import Vue from 'vue';
+import StarRating from 'vue-star-rating';
+
+const wrapper = document.createElement('div');
+// shared state
+const state = {
+  note: 0,
+};
+// crate component to content
+const RatingComponent = Vue.extend({
+  data() {
+    return { rating: 0 };
+  },
+  watch: {
+    rating(newVal) { state.note = newVal; },
+  },
+  template: `
+    <div class="rating">
+      How was your experience getting help with this issues?
+      <star-rating v-model="rating" :show-rating="false"></star-rating>
+    </div>`,
+  components: { 'star-rating': StarRating },
+});
+
+const component = new RatingComponent().$mount(wrapper);
 
 export default {
   name: 'Movie',
@@ -30,6 +56,35 @@ export default {
     this.fetchMovie();
   },
   methods: {
+    async rate() {
+      this.$swal({
+        content: component.$el,
+        buttons: {
+          confirm: {
+            value: 0,
+          },
+        },
+      }).then(() => {
+        const movieId = this.$route.params.id;
+        return axios({
+          method: 'post',
+          data: {
+            rate: state.note,
+          },
+          url: `http://localhost:8081/movies/rate/${movieId}`,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+          .then(() => {
+            this.$swal(`Thank you for rating! ${state.note}`, 'success');
+          })
+          .catch((error) => {
+            const message = error.response.data.message;
+            this.$swal('Oh oo!', `${message}`, 'error');
+          });
+      });
+    },
     async fetchMovie() {
       return axios({
         method: 'get',
