526ee7d9f3f8dd382ea6d6791a8a3236ca56cfac
Arturo <arturo.alfaro87@gmail.com>
2018-07-13 13:24:39 -0400

    Modelo y controlador de User terminado,on register view.

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const bcryptjs = require('bcryptjs');
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
+
+
+//Toma el user object, y transforma la password en una hash encriptado y la guarda todo en la base de tdatos
+module.exports.createUser = (newUser, callback) => {
+  bcryptjs.genSalt(10, (err, salt) => {
+    bcryptjs.hash(newUser.password, salt, (error, hash) => {
+      // store the hashed password
+      const newUserResource = newUser;
+      newUserResource.password = hash;
+      newUserResource.save(callback);
+    });
+  });
+};
---
+++ src/components/Register.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-form v-model="valid" ref="form" lazy-validation>
+    <v-text-field
+      label="Name"
+      v-model="name"
+      required
+    ></v-text-field>
+    <v-text-field
+      label="Email"
+      v-model="email"
+      :rules="emailRules"
+      required
+    ></v-text-field>
+    <v-text-field
+      label="Password"
+      v-model="password"
+      required
+    ></v-text-field>
+    <v-text-field
+      name="input-7-1"
+      label="Confirm Password"
+      v-model="confirm_password"
+    ></v-text-field>
+    <v-btn
+      @click="submit"
+      :disabled="!valid"
+    >
+      submit
+    </v-btn>
+    <v-btn @click="clear">clear</v-btn>
+  </v-form>
+</template>
